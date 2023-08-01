a59c3ad24d6d99de4d8460b2e0b6dca79d9bc2e2
Arina <arinaligit@gmail.com>
2023-08-01 12:13:58 +0300

    add resources and user information

--- SurfTestTask/MainViewController.swift
+++ SurfTestTask/MainViewController.swift
@@ -8,12 +8,135 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    // MARK: - Variables
+    
+    private let scrollView: UIScrollView = {
+        let scroll = UIScrollView()
+        return scroll
+    }()
+    
+    private let contentView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    private let photo: UIImageView = {
+        let photo = UIImageView()
+        photo.contentMode = .scaleAspectFit
+        photo.layer.cornerRadius = 25
+        photo.image = UIImage(systemName: "questionmark")
+        return photo
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .center
+        label.textColor = Resourses.Colors.textColor
+        label.font = Resourses.Fonts.helveticaRegular(with: 24)
+//        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        label.text = Resourses.Texts.name
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .center
+        label.textColor = Resourses.Colors.textDescription
+        label.font = Resourses.Fonts.helveticaRegular(with: 14)
+//        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        label.text = Resourses.Texts.description
+        return label
+    }()
+    
+    private let locationMark: UIImageView = {
+        let image = UIImageView()
+        image.image = Resourses.Images.location
+        return image
+    }()
+    
+    private let locationLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .center
+        label.textColor = Resourses.Colors.textDescription
+        label.font = Resourses.Fonts.helveticaRegular(with: 14)
+        label.text = Resourses.Texts.location
+        return label
+    }()
+    
+    private lazy var hStack: UIStackView = {
+        let hStack = UIStackView(arrangedSubviews: [locationMark, locationLabel])
+        hStack.axis = .horizontal
+        hStack.spacing = 2
+        hStack.distribution = .fill
+        hStack.alignment = .center
+        return hStack
+    }()
+    
+    private lazy var vStack: UIStackView = {
+        let vStack = UIStackView(arrangedSubviews: [nameLabel, descriptionLabel, hStack])
+        vStack.axis = .vertical
+        vStack.spacing = 4
+        vStack.distribution = .fill
+        vStack.alignment = .center
+        return vStack
+    }()
+    
+    // MARK: - LifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
+        
+        setView()
+        setConstraints()
+        
+        view.backgroundColor = Resourses.Colors.background
+        navigationItem.title = Resourses.Texts.profile
+    }
+    
+    private func setView() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubview(photo)
+        contentView.addSubview(vStack)
     }
 
+    private func setConstraints() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        photo.translatesAutoresizingMaskIntoConstraints = false
+        vStack.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
+            scrollView.heightAnchor.constraint(equalTo:view.layoutMarginsGuide.heightAnchor),
+            scrollView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            
+            photo.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            photo.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+            photo.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            photo.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            photo.heightAnchor.constraint(equalToConstant: 64),
+            
+            vStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            vStack.topAnchor.constraint(equalTo: photo.bottomAnchor, constant: 20),
+            vStack.leadingAnchor.constraint (equalTo: contentView.leadingAnchor),
+            vStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            vStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
 
 }
 
---
+++ SurfTestTask/Resources.swift
@@ -0,0 +1,76 @@
+//
+//  Resources.swift
+//  SurfTestTask
+//
+//  Created by Arina on 01.08.2023.
+//
+
+import UIKit
+
+enum Resourses {
+    enum Fonts {
+        static func helveticaRegular(with size: CGFloat) -> UIFont {
+            UIFont(name: "Helvetica", size: size) ?? UIFont()
+        }
+        
+        static func SFProDisplay(with size: CGFloat) -> UIFont {
+            UIFont(name: "14_20_Regular SF", size: size) ?? UIFont()
+        }
+    }
+    
+    enum Texts {
+        static var profile = "Профиль"
+        static var name = "Иванов Иван Иванович"
+        static var description = "Middle iOS-разработчик, опыт более 2-х лет"
+        static var location = "Воронеж"
+        static var skills = "Мои навыки"
+        static var about = "О себе"
+        static var aboutText = "Experienced software engineer skilled in developing scalable and maintainable systems"
+    }
+    
+    enum Colors {
+        static var background = UIColor(hexString: "#F3F3F5")
+        static var textColor = UIColor(hexString: "#313131")
+        static var textDescription = UIColor(hexString: "#96959B")
+    }
+    
+    enum Images {
+        static var location = UIImage(named: "location")?.resized(to: CGSize(width: 16, height: 16))
+        static var pencil = UIImage(named: "pencil")?.resized(to: CGSize(width: 24, height: 24))
+        static var checkmark = UIImage(named: "checkmark")?.resized(to: CGSize(width: 24, height: 24))
+        static var cross = UIImage(named: "cross")?.resized(to: CGSize(width: 14, height: 14))
+    }
+}
+
+//MARK: Меняем размер загруженных иконок через растровые изображения, используя метод UIGraphicsImageRenderer
+extension UIImage {
+    func resized(to newSize: CGSize) -> UIImage? {
+        let renderer = UIGraphicsImageRenderer(size: newSize)
+        let resizedImage = renderer.image { context in
+            self.draw(in: CGRect(origin: .zero, size: newSize))
+        }
+        return resizedImage
+    }
+}
+
+//MARK: Ext for hex color values
+extension UIColor {
+    convenience init(hexString: String) {
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt64()
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+    }
+}
+
