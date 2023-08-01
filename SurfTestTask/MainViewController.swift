6df89ff5f15e0f52c24a6f1d8b6190ec3ba6a556
Arina <arinaligit@gmail.com>
2023-08-01 10:42:41 +0300

    initial commit

---
+++ SurfTestTask/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  SurfTestTask
+//
+//  Created by Arina on 01.08.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
